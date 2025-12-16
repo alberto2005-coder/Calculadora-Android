18ff81040cf24800be4ca8b83b843dc5f22d09e9
Alberto Ortiz <albertoortixsanchez@gmail.com>
2025-12-16 12:35:16 +0100

    UDT: Botones

---
+++ Calculadora figma/src/app/App.tsx
@@ -0,0 +1,9 @@
+import { Calculator } from './components/Calculator';
+
+export default function App() {
+  return (
+    <div className="size-full flex items-center justify-center bg-[#1a1a1a]">
+      <Calculator />
+    </div>
+  );
+}
